c4cdb6e9516ab5e1936da82a8c0555c8386a3693
srkpolisetti <polisettisiva.siva@gmail.com>
2018-04-25 11:55:59 +0530

    perl test1 file

---
+++ test1.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+#conditional statements example
+
+#if statement
+$n1 = 11;
+if($n1 > 10){
+ print "if block is executed...\n";
+ print "\tnumber greater than 10\n";
+} #end if
+
+#if-else statement
+$n2 = 9;
+if($n2 > 10){
+ print "if block is executed...\n";
+ print "\tnumber greater than 10\n";
+}
+else{
+ print "else block is executed...\n";
+ print "\tnumber less than 10\n";
+} #end if
+
+#if-else-if statement
+$n3 = 10;
+if($n3 > 10){
+ print "if block is exucuted...\n";
+ print "\tnumber greater than 10\n";
+}
+elsif($n3 = 10){
+ print "else-if block is exucuted...\n";
+ print "\tnumber is equal to 10\n";
+}
+else{
+ print "else block is exucuted...\n";
+ print "\tnumber less than 10\n";
+} #end if 
+
+
+
+
+
